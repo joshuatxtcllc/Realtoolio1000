d2a1a094fb395bac75fecc775d29b34ddd9d6f75
joshuatxtcllc <jaysframes@gmail.com>
2025-10-28 17:34:53 -0500

    Create SkipTraceLeadScorer.js

---
+++ src/scoring/SkipTraceLeadScorer.js
@@ -0,0 +1,9 @@
+/**
+ * Skip Trace Lead Scoring Engine
+ * Scores leads based on distress indicators and likelihood to sell
+ */
+class SkipTraceLeadScorer {
+    // [Your SkipTraceLeadScorer class code]
+}
+
+export default SkipTraceLeadScorer;
