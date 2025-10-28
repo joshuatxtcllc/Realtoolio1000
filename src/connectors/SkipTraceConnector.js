0ee80742ceaef2b46c20c02fba36e36eeab8cf2e
joshuatxtcllc <jaysframes@gmail.com>
2025-10-28 17:34:20 -0500

    Create SkipTraceConnector.js

---
+++ src/connectors/SkipTraceConnector.js
@@ -0,0 +1,9 @@
+/**
+ * Google Sheets Skip Trace Data Connector
+ * Fetches and normalizes skip trace data from Google Sheets
+ */
+class SkipTraceConnector {
+    // [Your SkipTraceConnector class code]
+}
+
+export default SkipTraceConnector;
